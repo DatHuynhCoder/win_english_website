a5d50b40508cf6c25299ccda07e262f84cdb1c11
DatHuynhCoder <htdat2004@gmail.com>
2024-10-29 08:37:27 +0700

    Chinh sua app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import Footer from "./components/Footer/Footer";
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header'
+
+import { 
+  Outlet 
+} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="App">
-      Hello world
+    <div className="App-container">
+      <div className="Header-container">
+        <Header/>
+      </div>
+      <div className="Main-content">
+        <Outlet/>
+      </div> 
       <div className="footer-container">
-        <Footer />
+        <Footer/>
       </div>
     </div>
-
   );
 }
 
