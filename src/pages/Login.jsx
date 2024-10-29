46088a7c764d3c1065ed807af581f82ae9897bd0
Hatsuki9813 <22520600@gm.uit.edu.vn>
2024-10-29 09:59:54 +0700

    Add login.jsx file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './LoginSignupStyles.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-export default function Login() {
+const Login = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className={`container ${isActive ? 'active' : ''}`}>
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="repassword" placeholder="Confirm password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div className="forget-password">
+            <a href="#">Forget Your Password?</a>
+          </div>
+          <span>or using</span>
+          <div className="social-icons">
+            <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div>
+          <button onClick={handleLoginClick}>Sign In</button>
+        </form>
+      </div>
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of the site’s features</p>
+            <button className="hidden" onClick={handleLoginClick}>Sign In</button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>Register with your personal details to use all of the site’s features</p>
+            <button className="hidden" onClick={handleRegisterClick}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
