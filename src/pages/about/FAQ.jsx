53ad505e85168948eb7bd7d71f5cbf6905735f9b
DatHuynhCoder <htdat2004@gmail.com>
2024-10-30 12:30:42 +0700

    Tao thanh cong trang about

--- src/pages/about/FAQ.jsx
+++ src/pages/about/FAQ.jsx
@@ -6,13 +6,15 @@ import Accordion from 'react-bootstrap/Accordion';
 
 import FAQPic from '../../assets/faq_pic.svg'
 
+import './FAQ.scss'
+
 const FAQ = ({ faqData }) => {
     return (
         <div className="faq-container">
             <h2 className="faq-title">Bạn có câu hỏi nào không ?</h2>
             <Image src={FAQPic} alt='FAQ logo' className='faq-logo' fluid />
             <div className="faq-common-text">Câu hỏi thường gặp</div>
-            <Accordion>
+            <Accordion alwaysOpen className='accordion'>
                 {faqData.map((item, index) => (
                     <Accordion.Item eventKey={index.toString()} key={index}>
                         <Accordion.Header>{item.question}</Accordion.Header>
