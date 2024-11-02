3c206ccb9f8e32c02266b11a88a377999696ef14
NATuanAN <anhtuan2810999@gmail.com>
2024-11-02 13:18:53 +0700

    Thay doi file react

--- src/pages/payment/Payment.jsx
+++ src/pages/payment/Payment.jsx
@@ -9,31 +9,63 @@ const Payment= () => {
     const registeredStudents = "98,671";
     const topics = "95 chủ đề và hơn 1,028 bài học";
     const exercises = "2,416 bài tập thực hành";
+    const dangki = () => {
+        alert('Đăng ký học ngay!');
+    };
 
+    const hocthu = () => {
+        alert('Thử gói trong 3 ngày!');
+    };
     return (
+        <section>
         <div className="payment-card">
-            <h1>Gói đặc biệt</h1>
+            <h1>Gói đặc biệt theo tháng</h1>
             <p>Ưu đãi nhiều hơn</p>
             <div className="price">
-                <span className="discounted-price">{discountedPrice}</span>
+                <span className="discounted-price">999.000đ</span>
                 <span className="original-price">Giá gốc: {originalPrice}</span>
                 <span className="savings">{savings}</span>
             </div>
             
             <button className="register-button" id="register">ĐĂNG KÝ HỌC NGAY</button>
-            <button className="trial-button" id="Trying">Học thử miễn phí</button>
+            <button className="trial-button" id="Trying">Thử gói trong 3 ngày</button>
             
             <ul className="course-info">
                 <li>{registeredStudents} học viên đã đăng ký</li>
                 <li>{topics}</li>
                 <li>{exercises}</li>
-                <li>Có thể học trên nhiều nền tản</li>
+                <li>Có thể học trên nhiều nền tảng</li>
             </ul>
             
             <p className="consultation">
                 Nếu chưa chắc chắn khóa học này dành cho bạn có thể<a href="#">Liên hệ để nhận tư vấn miễn phí!</a>
             </p>
         </div>
+        <div className="payment-card">
+        <h1>Gói đặc biệt theo năm</h1>
+        <p>Ưu đãi nhiều hơn</p>
+        <div className="price">
+            <span className="discounted-price">{discountedPrice}</span>
+            <span className="original-price">Giá gốc: {originalPrice}</span>
+            <span className="savings">{savings}</span>
+        </div>
+        
+        <button className="register-button" id="register"  onClick={dangki}>ĐĂNG KÝ GÓI NGAY</button>
+        <button className="trial-button" id="Trying"  onClick={hocthu}>Thử gói trong 3 ngày</button>
+        
+        <ul className="course-info">
+            <li>{registeredStudents} học viên đã đăng ký</li>
+            <li>{topics}</li>
+            <li>{exercises}</li>
+            <li>Có thể học trên nhiều nền tảng</li>
+        </ul>
+        
+        <p className="consultation">
+            Nếu chưa chắc chắn khóa học này dành cho bạn có thể<a href="#">Liên hệ để nhận tư vấn miễn phí!</a>
+        </p>
+    </div>
+        </section>
+        
     );
 };
 
