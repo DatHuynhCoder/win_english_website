99157360b768ac1109bc2157fecec49437922a71
NATuanAN <anhtuan2810999@gmail.com>
2024-11-04 13:29:48 +0700

    Thay doi lan cuoi

--- src/pages/payment/Payment.jsx
+++ src/pages/payment/Payment.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Payment.css';
-import QR from './QRthanhtoan.png'
+import QR from'./QRthanhtoan.png'
 import bank from './bank.png'
 function Payment() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -47,14 +47,14 @@ function Payment() {
                 <div className="payment-card">
                     <h2>Quét mã QR để thanh toán</h2>
                     <nav>
-                        <img src={QR} alt="logo" />
+                    <img src={QR}  alt="logo" />
                     </nav>
                     <p>Mở các ứng dụng có VietQR để thanh toán</p>
                     <br />
                     <img src={bank} alt="bank-img" />
                     <br />
                     <button id="payment-button" onClick={handleOpenModal}>
-                        Hướng dẫn thanh toán
+                        Hướng dẫn thanh toán ?
                     </button>
                 </div>
             </section>
@@ -62,22 +62,32 @@ function Payment() {
             {/* Conditionally add "show" class based on isModalOpen */}
             <div className={`overlay ${isModalOpen ? 'show' : ''}`} onClick={handleCloseModal}></div>
             <div className={`modal ${isModalOpen ? 'show' : ''}`}>
+                <div className='thongtin'>
+                    
                 <h2>Thông tin đơn hàng</h2>
                 <h1>Gói Premium nâng cấp</h1>
-                <p>Giá trị đơn hàng: 3.999.999đ</p>
-                <p>Số tiền thanh toán: 3.999.999đ</p>
-                <br />
-                <p>Mã giao dịch</p>
-                <p>39568845698826597028776</p>
-                <br />
+                <section className='container' >
+                <div className='smallblock1'>
+                    <p>Giá trị đơn hàng: </p>
+                    <p>Số tiền thanh toán: </p>
+                    <p>Mã giao dịch</p>
+                    <br />
                 <p>Nội dung</p>
+                </div>
+                <div className='smallblock2'>
+                    <p>3.999.999đ</p>
+                    <p>3.999.999đ</p>
+                    <p>39568845698826597028776</p>
+                </div>
+                </section>
+                
                 <input type="text" />
+                </div>
                 <div className="timer" id="countdown-timer">
                     {countdownTime > 0
                         ? `Giao dịch kết thúc sau ${formatTime(countdownTime)}`
                         : "Giao dịch đã hết hạn"}
                 </div>
-
             </div>
         </div>
     );
